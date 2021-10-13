7d9c84a99ea8d00374e6d6bd649b2c52ca25b18c
anshulbansal17 <your_email_address@example.com>
2021-10-13 19:28:10 +0530

    day-4

--- Main Folder/Heaps/10_KthSmallestElementStoredInMatrix.cpp
+++ Main Folder/Heaps/10_KthSmallestElementStoredInMatrix.cpp
@@ -1,13 +1,28 @@
 // 10_KthSmallestElementStoredInMatrix.cpp
 // https://leetcode.com/problems/kth-smallest-element-in-a-sorted-matrix/
 
-// TC - O(M*N + KlogK)
-// SC - O()
+// TC - O(MN + MNLogMN)
+// SC - O(MN)
 #include <bits/stdc++.h>
 using namespace std;
-typedef pair<int,pair<int,int>> customPair;
 
-class Solution {
+class Solution1 {
+public:
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        priority_queue<int, vector<int>, greater<int>> pq;
+        for(auto row : matrix) 
+            for(auto el : row)
+                pq.push(el); 
+
+        k -= 1;
+        while(!pq.empty() and k--) pq.pop();
+        return pq.empty() ? -1 : pq.top();
+    }
+};
+
+
+typedef pair<int,pair<int,int>> customPair;
+class Solution2 {
 public:
     int kthSmallest(vector<vector<int>>& matrix, int k) {
         priority_queue<customPair,vector<customPair>,greater<customPair>> pq;
@@ -20,10 +35,11 @@ public:
             customPair curr = pq.top();
             pq.pop();
             k--;
+            // cout<<curr.first<<" ";
             if(k == 0) return curr.first;
             int ii = curr.second.first;
             int jj = curr.second.second;
-            if(jj+1 < n) pq.push({matrix[ii][jj+1],{ii,jj+1}});
+            if(jj+1 < n)pq.push({matrix[ii][jj+1],{ii,jj+1}});
         }
         return matrix[n-1][n-1];
     }
