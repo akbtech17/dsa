80bcec293c0d14c4da735ca8859b6674a9c96fe0
akb.tech17 <akb.tech17@gmail.com>
2025-03-19 20:50:57 +0530

    Refactor nextLargerElement function for improved performance and readability

--- TUF/Stack and Queue/Monotonic Stack/1_NextGreaterElement.cpp
+++ TUF/Stack and Queue/Monotonic Stack/1_NextGreaterElement.cpp
@@ -6,21 +6,21 @@ using namespace std;
 // SC: O(1)
 class Solution1 {
 public:
-    vector<int> nextLargerElement(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> ans(n, -1);
+  vector<int> nextLargerElement(vector<int> &nums) {
+    int n = nums.size();
+    vector<int> ans(n, -1);
 
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (nums[j] > nums[i]) {
-                    ans[i] = nums[j];
-                    break;
-                }
-            }
+    for (int i = 0; i < n; i++) {
+      for (int j = i + 1; j < n; j++) {
+        if (nums[j] > nums[i]) {
+          ans[i] = nums[j];
+          break;
         }
-
-        return ans;
+      }
     }
+
+    return ans;
+  }
 };
 
 // Approach2: Monotonic Stack
@@ -28,30 +28,32 @@ public:
 // SC: O(N)
 class Solution2 {
 public:
-    vector<int> nextLargerElement(vector<int>& nums) {
-        int n = nums.size();
-        stack<int> stk;
-        vector<int> ans(n, -1);
+  vector<int> nextLargerElement(vector<int> &nums) {
+    int n = nums.size();
+    stack<int> stk;
+    vector<int> ans(n, -1);
 
-        for (int i = n - 1; i >= 0; i--) {
-            // 1. Pop all elements smaller and equal than current element
-            while (!stk.empty() && stk.top() <= nums[i]) stk.pop();
-            // 2. Store the answer
-            ans[i] = stk.empty() ? -1 : stk.top();
-            // 3. Push the current element
-            stk.push(nums[i]);
-        }
-
-        return ans;
+    for (int i = n - 1; i >= 0; i--) {
+      // 1. Pop all elements smaller and equal than current element
+      while (!stk.empty() && stk.top() <= nums[i])
+        stk.pop();
+      // 2. Store the answer
+      ans[i] = stk.empty() ? -1 : stk.top();
+      // 3. Push the current element
+      stk.push(nums[i]);
     }
+
+    return ans;
+  }
 };
 
 int main() {
-    Solution2 sol;
-    vector<int> nums = { 1, 3, 2, 4 };
+  Solution2 sol;
+  vector<int> nums = {1, 3, 2, 4};
 
-    vector<int> ans = sol.nextLargerElement(nums);
-    for (int el : ans) cout << el << " ";
+  vector<int> ans = sol.nextLargerElement(nums);
+  for (int el : ans)
+    cout << el << " ";
 
-    return 0;
+  return 0;
 }
