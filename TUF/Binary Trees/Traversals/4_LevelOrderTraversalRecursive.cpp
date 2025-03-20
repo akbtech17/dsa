973e05f26968ee9feab0b4124ca5f980cad1124d
akb.tech17 <akb.tech17@gmail.com>
2025-03-20 09:56:37 +0530

    Add iterative level order traversal for binary trees

---
+++ TUF/Binary Trees/Traversals/4_LevelOrderTraversalRecursive.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+struct TreeNode {
+  public:
+	TreeNode *left, *right;
+	int data;
+};
+
+class Solution {
+  public:
+	// TC: O(N)
+	// SC: O(H)
+	void recurse(TreeNode *root, vector<vector<int>> &traversal, int d = 0) {
+		if (!root)
+			return;
+
+		if (traversal.size() <= d) {
+			traversal.resize(d + 1);
+		}
+		traversal[d].push_back(root->data);
+
+		recurse(root->left, traversal, d + 1);
+		recurse(root->right, traversal, d + 1);
+
+		return;
+	}
+
+	vector<vector<int>> levelOrder(TreeNode *root) {
+		vector<vector<int>> traversal;
+		recurse(root, traversal);
+		return traversal;
+	}
+};
