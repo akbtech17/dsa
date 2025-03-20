26c77b97cbf2fe0affb7a1351636da7f32f596d3
akb.tech17 <akb.tech17@gmail.com>
2025-03-20 09:09:16 +0530

    Add iterative inorder traversal for binary trees

---
+++ TUF/Binary Trees/Traversals/1_InorderIterative.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+struct TreeNode {
+  public:
+	TreeNode *left, *right;
+	int data;
+};
+
+class Solution {
+  public:
+	// TC: O(N)
+	// SC: O(H)
+	vector<int> inorder(TreeNode *root) {
+		vector<int> ans;
+		stack<TreeNode *> stk;
+
+		while (root || !stk.empty()) {
+			if (root != nullptr) {
+				// Left
+				stk.push(root);
+				root = root->left;
+			} else {
+				// Display
+				TreeNode *node = stk.top();
+				stk.pop();
+				ans.push_back(node->data);
+				// Right
+				root = node->right;
+			}
+		}
+
+		return ans;
+	}
+};
