943b20ed9b07e24999b2fce4f7b88a305dce2c45
akb.tech17 <akb.tech17@gmail.com>
2025-03-20 10:58:54 +0530

    Refactor max depth calculation in binary tree solution

--- TUF/Binary Trees/Medium Problems/1_MaxDepthBT.cpp
+++ TUF/Binary Trees/Medium Problems/1_MaxDepthBT.cpp
@@ -7,7 +7,8 @@ public:
 	int data;
 };
 
-class Solution {
+// Approach1: Recursive
+class Solution1 {
 public:
 	// TC: O(N)
 	// SC: O(H)
@@ -18,5 +19,34 @@ public:
 		int right_ht = maxDepth(root->right);
 
 		return 1 + max(left_ht, right_ht);
+	}
+};
+
+// Approach2: Iterative using LOT
+class Solution2 {
+public:
+	// TC: O(N)
+	// SC: O(W)
+	int maxDepth(TreeNode *root) {
+		if (!root) return 0;
+
+		int levels = 0;
+		queue<TreeNode *> q;
+		q.push(root);
+
+		while (!q.empty()) {
+			int qs = q.size();
+
+			for (int i = 0; i < qs; i++) {
+				TreeNode *node = q.front();
+				q.pop();
+
+				if (node->left) q.push(node->left);
+				if (node->right) q.push(node->right);
+			}
+			levels++;
+		}
+
+		return levels;
 	}
 };
