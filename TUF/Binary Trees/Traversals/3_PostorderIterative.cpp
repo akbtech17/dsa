c73b9bbcb5892c414f64f2c98f8f0a2b622ac10d
akb.tech17 <akb.tech17@gmail.com>
2025-03-20 09:53:19 +0530

    Add .clang-format file for consistent code formatting

---
+++ TUF/Binary Trees/Traversals/3_PostorderIterative.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+struct TreeNode {
+  public:
+	TreeNode *left, *right;
+	int data;
+};
+
+class Solution {
+  public:
+	// TC: O(N)
+	// SC: O(H)
+	// Preorder 			Root->Left->Right
+	// Postorder 			Left->Right->Root
+	// Reverse Postorder 	Root->Right->Left
+	//[slight modification of Preorder then reverse]
+	vector<int> postorder(TreeNode *root) {
+		vector<int> ans;
+		if (!root) return ans;
+
+		stack<TreeNode *> stk;
+		stk.push(root);
+
+		while (!stk.empty()) {
+			TreeNode *node = stk.top();
+			stk.pop();
+
+			// Display
+			ans.push_back(node->data);
+			// Left
+			if (node->left) stk.push(node->left);
+			// Right
+			if (node->right) stk.push(node->right);
+		}
+
+		reverse(ans.begin(), ans.end());
+
+		return ans;
+	}
+};
