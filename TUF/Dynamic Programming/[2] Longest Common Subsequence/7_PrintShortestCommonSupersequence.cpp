2952052f01aeb0175baa66c08186fd6a57b5a7b5
akb.tech17 <akb.tech17@gmail.com>
2025-03-29 18:54:29 +0530

    Refactor Longest Common Subsequence algorithm for improved performance and readability

--- TUF/Dynamic Programming/[2] Longest Common Subsequence/7_PrintShortestCommonSupersequence.cpp
+++ TUF/Dynamic Programming/[2] Longest Common Subsequence/7_PrintShortestCommonSupersequence.cpp
@@ -6,7 +6,7 @@ using namespace std;
 // SC: O(N*M)
 class Solution {
 public:
-	string printLCS(string str1, string str2) {
+	string shortestCommonSupersequence(string str1, string str2) {
 		int l1 = str1.size(), l2 = str2.size();
 		vector<vector<int>> dp(l1 + 1, vector<int>(l2 + 1, 0));
 
@@ -21,20 +21,33 @@ public:
 
 		int lcsLen = dp[l1][l2];
 		string ans = "";
-		for(int i=0; i<lcsLen; i++) ans += '$';
-		int itr = lcsLen-1;
-		
+
 		// TC: O(N+M)
 		int i1 = l1, i2 = l2;
-		while(i1>0 && i2>0) {
-			if(str1[i1-1] == str2[i2-1]) {
-				ans[itr--] = str1[i1-1];
+		while (i1 > 0 && i2 > 0) {
+			if (str1[i1 - 1] == str2[i2 - 1]) {
+				ans += str1[i1 - 1];
 				i1--, i2--;
+			} else if (dp[i1][i2 - 1] > dp[i1 - 1][i2]) {
+				ans += str2[i2 - 1];
+				i2--;
+			} else {
+				ans += str1[i1 - 1];
+				i1--;
 			}
-			else if(dp[i1][i2-1] > dp[i1-1][i2]) i2--; 
-			else i1--;
 		}
 
+		while (i1 > 0) {
+			ans += str1[i1 - 1];
+			i1--;
+		}
+
+		while (i2 > 0) {
+			ans += str2[i2 - 1];
+			i2--;
+		}
+
+		reverse(ans.begin(), ans.end());
 		return ans;
 	}
 };
@@ -42,8 +55,8 @@ public:
 int main() {
 	string str1 = "abcde", str2 = "bdgek";
 	Solution sol;
-	string ans = sol.printLCS(str1, str2);
-	cout<<ans;
+	string ans = sol.shortestCommonSupersequence(str1, str2);
+	cout << ans;
 
 	return 0;
 }
