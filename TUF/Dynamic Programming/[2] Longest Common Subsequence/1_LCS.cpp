12b16ea195c91d1ff1414cf560c2885b1ee9a677
akb.tech17 <akb.tech17@gmail.com>
2025-03-29 12:11:26 +0530

    Refactor LCS algorithm for improved performance and readability

--- TUF/Dynamic Programming/[2] Longest Common Subsequence/1_LCS.cpp
+++ TUF/Dynamic Programming/[2] Longest Common Subsequence/1_LCS.cpp
@@ -68,7 +68,7 @@ public:
 
 		for (int i1 = 1; i1 <= l1; i1++) {
 			for (int i2 = 1; i2 <= l2; i2++) {
-				if (str1[i1-1] == str2[i2-1])
+				if (str1[i1 - 1] == str2[i2 - 1])
 					dp[i1][i2] = 1 + dp[i1 - 1][i2 - 1];
 				else
 					dp[i1][i2] = max(dp[i1 - 1][i2], dp[i1][i2 - 1]);
@@ -86,10 +86,9 @@ class Solution4 {
 public:
 	int lcs(string str1, string str2) {
 		int l1 = str1.size(), l2 = str2.size();
-		vector<int> prev(l2 + 1, 0);
+		vector<int> prev(l2 + 1, 0), curr(l2 + 1, 0);
 
 		for (int i1 = 1; i1 <= l1; i1++) {
-			vector<int> curr(l2 + 1, 0);
 			for (int i2 = 1; i2 <= l2; i2++) {
 				if (str1[i1 - 1] == str2[i2 - 1])
 					curr[i2] = 1 + prev[i2 - 1];
