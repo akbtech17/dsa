c7795831cc541cd5242a55d585b8db7f3c103d05
akb.tech17 <akb.tech17@gmail.com>
2025-05-22 09:47:37 +0530

    Implemented Topological Sort Algorithm

--- TUF/Graph/[3] - Cycles/3_TopologicalSort.cpp
+++ TUF/Graph/[3] - Cycles/3_TopologicalSort.cpp
@@ -36,3 +36,41 @@ public:
 		return ans;
 	}
 };
+
+// Approach: DFS
+// TC: O(V+E)
+// SC: O(V)
+class Solution {
+public:
+	void dfs(int node, vector<int> *adj, vector<bool> &isvis, stack<int> &stk) {
+		isvis[node] = true;
+
+		for (int nbr : adj[node]) {
+			if (isvis[nbr] == false) dfs(nbr, adj, isvis, stk);
+		}
+
+		// once the outdegrees of the currNode is processed
+		stk.push(node);
+	}
+
+	vector<int> topoSort(int V, vector<int> adj[]) {
+		vector<int> ans;
+		vector<bool> isvis(V, false);
+		stack<int> stk;
+
+		for (int i = 0; i < V; i++) {
+			if (isvis[i] == false) {
+				dfs(i, adj, isvis, stk);
+			}
+		}
+
+		while (!stk.empty()) {
+			int node = stk.top();
+			stk.pop();
+
+			ans.push_back(node);
+		}
+
+		return ans;
+	}
+};
